d326415d967d8d68abff6b07c5115a9cf248422f
RybakAndrey <andrey_0895@mail.ru>
2016-04-22 00:36:27 +0300

    Task5 быдлокод

--- RybakAndrey/Task 5 Smart plant eater/script.js
+++ RybakAndrey/Task 5 Smart plant eater/script.js
@@ -1,39 +1,28 @@
 function startPlanet() {
     var planet = getPlanet();
-    planet = move(planet);
-    renderMatrix(planet)
-}
-
-function renderMatrix(planet) {
-    var rootDiv = document.getElementsByClassName("matrix")[0];
-    var table = document.createElement("table");
-    for (var i = 0; i < planet.length; i++) {
-        var row = document.createElement("tr");
-        for (var j = 0; j < planet[0].length; j++) {
-            var column = document.createElement("td");
-            column.textContent = planet[i][j];
-            row.appendChild(column);
-        }
-        table.appendChild(row);
-    }
-    rootDiv.appendChild(table);
+    setInterval(function (){
+        planet = move(planet);
+        renderMatrix(planet);
+    },100);
 }
 
 
 function getPlanet() {
     return       [  "############################",
-                    "#####WWWWWWWWWWWRRRRRR######",
-                    "##   ***                **##",
-                    "#   *##** W       **  R  *##",
-                    "#    ***     R    ##**    *#",
-                    "#       R         ##***    #",
-                    "#                 ##**     #",
-                    "#   R       #*             #",
-                    "#*      W   #**       R    #",
-                    "#***        ##**    R    **#",
-                    "##****     ###***       *###",
+                    "#####  R  W  W  RRR  R######",
+                    "##   ***       RRR WWW  **##",
+                    "#   *##** W    W  **  R  *##",
+                    "#  R ***WWW  R    ##**    *#",
+                    "# R R W WRW       ##***    #",
+                    "# W   R  R WWW    ##** W   #",
+                    "#   R   W   #*   R  R W R  #",
+                    "#*  W   W   #**    W  R    #",
+                    "#***   RRR   ##**    R   **#",
+                    "##****     ###***  RW   *###",
                     "############################"];
 }
+
+
 
 function move(planet) {
     var tempPlanet = planet.slice();
@@ -62,9 +51,23 @@ function move(planet) {
     return planet;
 }
 
-function generateRabbit(planet){
 
+function checkStep(tempPlanet,animal) {
+    if(animal=="Rabbit") {
+        if (tempPlanet == "#" || tempPlanet=="W" || tempPlanet=="R") {
+            return false;
+        }else{
+            return true;
+        }
+    }else if(animal=="Wolf"){
+        if (tempPlanet == "#" || tempPlanet=="W") {
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
+
 
 function makeMove(tempPlanet,i,j,x,y,animal) {
     var symbol1, symbol2;
@@ -88,21 +91,24 @@ function makeMove(tempPlanet,i,j,x,y,animal) {
 }
 
 
-function checkStep(tempPlanet,animal) {
-    if(animal=="Rabbit") {
-        if (tempPlanet == "#" || tempPlanet=="W" || tempPlanet=="R") {
-            return false;
-        }else{
-            return true;
+function renderMatrix(planet) {
+    var rootDiv = document.getElementsByClassName("matrix")[0];
+    var table = document.createElement("table");
+    for (var i = 0; i < planet.length; i++) {
+        var row = document.createElement("tr");
+        for (var j = 0; j < planet[0].length; j++) {
+            var column = document.createElement("td");
+            column.textContent = planet[i][j];
+            row.appendChild(column);
         }
-    }else if(animal=="Wolf"){
-        if (tempPlanet == "#" || tempPlanet=="W") {
-            return false;
-        }else{
-            return true;
-        }
+        table.appendChild(row);
     }
+    if(rootDiv.childNodes.length>0){
+        rootDiv.removeChild(rootDiv.childNodes[0]);
+    }
+    rootDiv.appendChild(table);
 }
+
 
 function getRandomInt(min, max)
 {
